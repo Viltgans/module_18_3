9cc8776df92bb4aa114984247589a0dfeaf9791b
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-12-19 05:15:19 +0300

    Add files via upload

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def class_view(request):
+    return render(request, 'class_template.html')
+
+def func_view(request):
+    return render(request, 'func_template.html')
---
+++ UrbanDjango/task3/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main_page(request):
+    title = 'Главная страница'
+    text = 'Главная'
+    shop = 'Магазин'
+    cart = 'Корзина'
+    context = {
+        'title': title,
+        'text': text,
+        'shop': shop,
+        'cart': cart
+    }
+    return render(request, 'main_page.html', context)
+
+def first_additional_page(request):
+    title = 'Магазин'
+    text = 'Игры'
+    buy = 'Купить'
+    back = 'Вернуться обратно'
+    game_1 = "Baldur's Gate III"
+    game_2 = 'Cyberpunk 2077'
+    game_3 = 'Dragon Age Veilguard'
+    context = {
+        'title': title,
+        'text': text,
+        'buy': buy,
+        'back': back,
+        'game_1': game_1,
+        'game_2': game_2,
+        'game_3': game_3,
+    }
+    return render(request, 'first_additional_page.html', context)
+
+def second_additional_page(request):
+    title = 'Корзина'
+    text = "Извините, Ваша корзина пуста"
+    back = 'Вернуться обратно'
+    context = {
+        'title': title,
+        'text': text,
+        'back': back
+    }
+    return render(request, 'second_additional_page.html', context)
